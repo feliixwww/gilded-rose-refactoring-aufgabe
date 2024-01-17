4fa8f062d9267b55d2797228d604409b2679e4a5
feliixwww <felix@soul-werner.de>
2024-01-17 14:52:53 +0100

    def hinzugefügt

--- gilded_rose_besser.py
+++ gilded_rose_besser.py
@@ -1,32 +1,49 @@
+def standard_abzug(item):
+    if item.quality > 0:
+        item.quality = item.quality - 1
+        if item.sell_in <= 0:
+            item.quality = item.quality - 1
+
+def brie_gut_gealtert(item):
+    item.quality = item.quality + 1
+    if item.sell_in <= 0 and item.quality < 50:
+        item.quality = item.quality + 1
+
+
+
+
+
+
+
+
+
+
+
+def backstage_anforderung(item):
+    item.quality = item.quality + 1
+    if 11 > item.sell_in >= 0:
+        if item.quality < 50:
+            item.quality = item.quality + 1
+    if 6 > item.sell_in >= 0:
+        if item.quality < 50:
+            item.quality = item.quality + 1
+
 
 class GildedRose(object):
-
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-
             if (item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name !=
                     "Conjured Mana Cake" and item.name != "Sulfuras, Hand of Ragnaros"):
-                if item.quality > 0:
-                    item.quality = item.quality - 1
-                    if item.sell_in <= 0:
-                        item.quality = item.quality - 1
+                standard_abzug(item)
 
             if item.name == "Aged Brie" and item.quality < 50:
-                item.quality = item.quality + 1
-                if item.sell_in <= 0 and item.quality < 50:
-                    item.quality = item.quality + 1
+                brie_gut_gealtert(item)
 
             if item.name == "Backstage passes to a TAFKAL80ETC concert" and item.quality < 50:
-                item.quality = item.quality + 1
-                if 11 > item.sell_in >= 0:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                if 6 > item.sell_in >= 0:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                backstage_anforderung(item)
 
             if item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.sell_in <= 0:
